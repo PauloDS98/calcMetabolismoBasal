62d2a29cf522ce28c11d7dabf4c6cfff162e5531
RyanV-Souza <ryanvsouza99@gmail.com>
2019-08-12 05:50:14 +0900

    Adicionando função do botão Mulher e estilizando página escondida \(JS/CSS\)

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,29 @@
-// This is a JavaScript file
+var mulher = false;
+var homem = false;
+
+$(document).ready(function(){
+    
+
+    $(".confirmMulher").hide();
+    $('.btnCancel').hide();
+    $(".confirmHomem").hide();
+    $(".img-mulher").hide();
+
+    
+
+    $(document).on("click", ".mulher", function(){
+      $("body").css("background-color", "pink");
+      $(".container").css("background-color", "pink");
+      $(".mulher").hide();
+      $(".homem").hide();
+      $(".texto").html("Você é uma mulher?")
+      $(".confirmMulher").show();
+      $('.btnCancel').show();
+      $(".img-mulher").show();
+    });
+
+
+ 
+});
+  
 
