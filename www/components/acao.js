4c04c2e2416c5800aa7b0ef17a80fe61132f1247
PauloDS98 <desenvolver.paulo98@gmail.com>
2019-08-13 05:37:48 +0900

    Concluindo as funções de escolha de atividade \(falta o confirma\)e adicionando alguns comentários para facilitar busca

--- www/components/acao.js
+++ www/components/acao.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     
 
     
-
+    //Função quando escolher o genero biologico Mulher
     $(document).on("click", ".mulher", function(){
       $("body").css("background-color", "pink");
       $(".container").css("background-color", "pink");
@@ -25,6 +25,7 @@ $(document).ready(function(){
       $(".img-mulher").show();
     });
 
+    //Função quando escolher o genero biologico Homem
     $(document).on("click", ".homem", function(){
       $("body").css("background-color", "#528ed3");
       $(".container").css("background-color", "#528ed3");
@@ -36,6 +37,7 @@ $(document).ready(function(){
       $(".img-homem").show();
     });
 
+    //Botão cancelar
     $(document).on("click", ".btnCancel", function(){
       $("body").css("background-color", "#d86565");
       $(".container").css("background-color", "whitesmoke");
@@ -49,16 +51,18 @@ $(document).ready(function(){
       $(".img-mulher").hide();
     })
 
+  //Confirmação de ser Mulher
     $(document).on("click", ".confirmMulher", function(){
       mulher = true;
       $(location).attr("href", "atividade.html");
     })
-
+  //Confirmação de ser Homem
     $(document).on("click", ".confirmHomem", function(){
       homem = true;
       $(location).attr("href", "atividade.html");
     })
 
+  //Botão de escolha sedentario
     $(document).on("click", ".btnSedentario", function(){
       $(".escolha").hide();
       $(".confirmLevemente").hide();
@@ -76,6 +80,7 @@ $(document).ready(function(){
       $(".textoAuxiliar").text("Sedentário");    
     })
 
+  //Botão de escolha levemente Ativo
     $(document).on("click", ".btnLAtivo", function(){
       $(".cancelAtv").show();
       $(".escolha").hide();
@@ -89,11 +94,69 @@ $(document).ready(function(){
       $(".quadro").css("top", "30vh");
       $(".quadro").css("left", "8.5vh");
       $(".quadro").css("border-radius", "20px");
-      $(".textoAuxiliarAtv").text("Exercícios leves, 1 a 3 dias");
+      $(".textoAuxiliarAtv").html("Exercícios leves, 1 a 3 dias");
       $(".textoAuxiliar").show();  
-      $(".textoAuxiliar").text("Levemente Atv.");    
+      $(".textoAuxiliar").html("Levemente Atv.");    
     })
 
+  //Botão de escolha Moderadamente Ativo
+    $(document).on("click", ".btnMAtivo", function(){
+      $(".cancelAtv").show();
+      $(".escolha").hide();
+      $(".confirmSedentario").hide();           
+      $(".confirmLevemente").show();         
+      $(".cancelAtv").show();
+      $(".quadro").css("background-color", "white");
+      $(".quadro").css("position", "fixed");
+      $(".quadro").css("width", "70%");
+      $(".quadro").css("height", "50%");
+      $(".quadro").css("top", "30vh");
+      $(".quadro").css("left", "8.5vh");
+      $(".quadro").css("border-radius", "20px");
+      $(".textoAuxiliarAtv").html("Exercícios moderado, faz esporte <br> de 3 a 5 dias por semana");
+      $(".textoAuxiliar").show();  
+      $(".textoAuxiliar").html("Moderadamente Atv.");    
+    })
+
+ //Botão de escolha Altamente Ativo
+    $(document).on("click", ".btnAAtivo", function(){
+      $(".cancelAtv").show();
+      $(".escolha").hide();
+      $(".confirmSedentario").hide();           
+      $(".confirmLevemente").show();         
+      $(".cancelAtv").show();
+      $(".quadro").css("background-color", "white");
+      $(".quadro").css("position", "fixed");
+      $(".quadro").css("width", "70%");
+      $(".quadro").css("height", "50%");
+      $(".quadro").css("top", "30vh");
+      $(".quadro").css("left", "8.5vh");
+      $(".quadro").css("border-radius", "20px");
+      $(".textoAuxiliarAtv").html("Exercicio pesado de 5 a 6 dias por <br> semana");
+      $(".textoAuxiliar").show();  
+      $(".textoAuxiliar").html("Altamente Atv.");    
+    })
+
+ //Botão d escolha Extremamente Ativo
+    $(document).on("click", ".btnEAtivo", function(){
+      $(".cancelAtv").show();
+      $(".escolha").hide();
+      $(".confirmSedentario").hide();           
+      $(".confirmLevemente").show();         
+      $(".cancelAtv").show();
+      $(".quadro").css("background-color", "white");
+      $(".quadro").css("position", "fixed");
+      $(".quadro").css("width", "70%");
+      $(".quadro").css("height", "50%");
+      $(".quadro").css("top", "30vh");
+      $(".quadro").css("left", "8.5vh");
+      $(".quadro").css("border-radius", "20px");
+      $(".textoAuxiliarAtv").html("Exercicio pesado diariamente e <br> até 2 vezes por dia");
+      $(".textoAuxiliar").show();  
+      $(".textoAuxiliar").html("Extremamente Atv.");    
+    })
+
+ //Botão para cancelar a atividade
     $(document).on("click", ".cancelAtv", function(){
       $(".escolha").show();
       $(".cancelAtv").hide();
