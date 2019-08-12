46b256a877ca9358e0f7b57fd3e5ae5e020a3001
RyanV-Souza <ryanvsouza99@gmail.com>
2019-08-13 04:53:18 +0900

    Criação da nova página de Atividades

--- www/components/acao.js
+++ www/components/acao.js
@@ -3,12 +3,14 @@ var homem = false;
 
 $(document).ready(function(){
     
-    $(".img-workout").hide();
     $(".confirmMulher").hide();
     $('.btnCancel').hide();
     $(".confirmHomem").hide();
     $(".img-mulher").hide();
     $(".img-homem").hide();
+    $(".cancelAtv").hide();
+
+    
 
     
 
@@ -35,7 +37,7 @@ $(document).ready(function(){
     });
 
     $(document).on("click", ".btnCancel", function(){
-      $("body").css("background-color", "grey");
+      $("body").css("background-color", "#d86565");
       $(".container").css("background-color", "whitesmoke");
       $(".mulher").show();
       $(".homem").show();
@@ -56,6 +58,65 @@ $(document).ready(function(){
       homem = true;
       $(location).attr("href", "atividade.html");
     })
+
+    $(document).on("click", ".btnSedentario", function(){
+      $(".btnSedentario").hide();
+      $(".confirmSedentario").show();         
+      $(".btn2").hide();
+      $(".btn3").hide();
+      $(".btn4").hide();
+      $(".btnEAtivo").hide();
+      $(".cancelAtv").show();
+      $(".quadro").css("background-color", "white");
+      $(".quadro").css("position", "fixed");
+      $(".quadro").css("width", "70%");
+      $(".quadro").css("height", "50%");
+      $(".quadro").css("top", "30vh");
+      $(".quadro").css("left", "8.5vh");
+      $(".quadro").css("border-radius", "20px");
+      $(".textoAuxiliarAtv").text("Fazem pouco ou nenhum exercício");
+      $(".textoAuxiliar").show();  
+      $(".textoAuxiliar").text("Sedentário");    
+    })
+
+    $(document).on("click", ".btnLAtivo", function(){
+      $(".escolha").hide();
+      $(".confirmLevemente").show();         
+      $(".btnEAtivo").hide();
+      $(".cancelAtv").show();
+      $(".quadro").css("background-color", "white");
+      $(".quadro").css("position", "fixed");
+      $(".quadro").css("width", "70%");
+      $(".quadro").css("height", "50%");
+      $(".quadro").css("top", "30vh");
+      $(".quadro").css("left", "8.5vh");
+      $(".quadro").css("border-radius", "20px");
+      $(".textoAuxiliarAtv").text("Exercícios leves, 1 a 3 dias por semana");
+      $(".textoAuxiliar").show();  
+      $(".textoAuxiliar").text("Levemente Ativo");    
+    })
+
+    $(document).on("click", ".cancelAtv", function(){
+      $(".btnSedentario").show();
+      $(".btn2").show();
+      $(".btn3").show();
+      $(".btn4").show();
+      $(".btnEAtivo").show();
+      $(".cancelAtv").hide();
+      $(".quadro").css("background-color", "");
+      $(".quadro").css("position", "");
+      $(".quadro").css("width", "");
+      $(".quadro").css("height", "");
+      $(".quadro").css("top", "");
+      $(".quadro").css("left", "");
+      $(".quadro").css("border-radius", "");
+      $(".textoAuxiliarAtv").text("");
+      $(".textoAuxiliar").hide();
+      $(".confirmSedentario").hide();
+      $(".confirmLevemente").hide();       
+    })
+
+
 
  
 });
